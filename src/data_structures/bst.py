51f39fc618d5c14beef3913170ee40fec1fd3c58
Fariz <fariz123@gmail.com>
2026-05-23 00:34:17 +0700

    update

--- src/data_structures/bst.py
+++ src/data_structures/bst.py
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.kiri = None
         self.kanan = None
-=======
 from dataclasses import dataclass, field
 from typing import List, Optional
->>>>>>> dev
-
 # Struktur Data Rekam Medis sesuai starter code
 @dataclass
 class RekorMedis:
